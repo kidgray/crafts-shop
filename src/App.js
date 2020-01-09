c6d6286c3f1cdf8c33d5e7eb7554d513e1d79af1
kidgray <micah.allauca@gmail.com>
2020-01-08 22:50:31 -0500

    Implemented some routing functionality w/ react-router-dom package.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
 
+/* Import Route */
+import { Switch, Route } from 'react-router-dom';
+
 /* Import HomePage */
 import { HomePage } from './pages/homepage/homepage.component';
 
+const DollsPage = () => (
+  <div>
+    <h1>DOLLS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
-    <HomePage />
+    <div>
+      {/* Switch component from react-router-dom package ensures
+      that as soon as a route inside of it finds a matching path,
+      we do not render anything but that route. */}
+      <Switch>
+        {/* // Use react-router-dom's Route Component
+        // to display the homepage */}
+        <Route exact={true} path='/' component={HomePage} />
+      </Switch>
+    </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+/* Import react-router-dom */
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    // BrowserRouter is a component that is 
+    // wrapped around an entire App and gives it 
+    // routing functionality.
+    <BrowserRouter>
+        <App />, 
+        document.getElementById('root')
+    </BrowserRouter>
+);

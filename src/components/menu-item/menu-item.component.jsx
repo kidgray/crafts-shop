12bf0b40ff31f3823509d662b599b5481ed20e95
kidgray <micah.allauca@gmail.com>
2020-01-08 21:30:28 -0500

    Some more styling work on menu items and the <body>

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -10,12 +10,18 @@ Note that I destructure the props for easier access and so I don't
 forget what I'm supposed to be passing in to this component later. */
 const MenuItem = ({ title, imageUrl, size }) => (
     /* Use React's built-in style attribute for JSX elements
-    to add a background image to each MenuItem element */
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`}>
+    to add a background image to each MenuItem element. Also
+    use template string to add in the size prop as a class */
+    <div className={`${size} menu-item`}>
+
+        {/* Image in the background of the tab */}
+        <div className='background-image' style={{
+            backgroundImage: `url(${imageUrl})`
+        }} />
+
+        {/* The text inside of the tab */}
         <div className='content'>
-            <h1 className='title'>{ title }</h1>
+            <h1 className='title'>{ title.toUpperCase() }</h1>
             <span className="subtitle">BROWSE SELECTION</span>
         </div>
     </div>

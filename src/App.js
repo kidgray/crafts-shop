30ab577a72f51e0eb78cc18ce7b96dd3b156fca6
kidgray <micah.allauca@gmail.com>
2020-01-12 02:34:18 -0500

    Some more work on Firebase Authentication w/ Google Sign-in. Added onAuthStateChanged() and Lifecycle methods to App.js

--- src/App.js
+++ src/App.js
@@ -16,33 +16,68 @@ import { SignInAndSignUpPage } from './pages/sign-in-and-sign-up/sign-in-and-sig
 /* Import Header */
 import { Header } from './components/header/header.component';
 
-function App() {
-  return (
-    <div>
+/* Import Authentication Utility from Firebase utilities */
+import { auth } from './firebase/firebase.utils';
 
-      {/* By placing the Header Component outside of the Switch/Route
-      Component tree, the site's Header will always be rendered no matter
-      which specific page is rendered by React. */}
-      <Header />
+/* App is now a Class Component, since I need to store the user info
+provided to Google/Facebook Authentication Utilities in the App's state
+so that it can later be passed to components that need it */
+class App extends React.Component {
+  
+  constructor(){
+    super();
 
-      {/* Switch component from react-router-dom package ensures
-      that as soon as a route inside of it finds a matching path,
-      we do not render anything but that route. */}
-      <Switch>
-        {/* // Use react-router-dom's Route Component
-        // to display pages */}
+    this.state = {
+      currentUser: null
+    }
+  }
 
-        {/* Home Page */}
-        <Route exact={true} path='/' component={HomePage} />
+  // Will be used to close the Open Subscription when the web app
+  // is unmounted. Closing the subscription will avoid memory leaks
+  unsubscribeFromAuth = null;
 
-        {/* Shop Page */}
-        <Route exact={true} path='/shop' component={ShopPage} />
+  /* 
+  Use the onAuthStateChanged() method of the auth library.
+  This method takes a function whose argument is the user of a
+  given Firebase authenticator. 
+  */
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => { this.setState({currentUser: user}) });
+  }
 
-        {/* Sign In/Sign Up Page */}
-        <Route exact={true} path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+  /* Whenever the App Component unmounts, close the Authentication Open Subscription */
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+  
+        {/* By placing the Header Component outside of the Switch/Route
+        Component tree, the site's Header will always be rendered no matter
+        which specific page is rendered by React. */}
+        <Header />
+  
+        {/* Switch component from react-router-dom package ensures
+        that as soon as a route inside of it finds a matching path,
+        we do not render anything but that route. */}
+        <Switch>
+          {/* // Use react-router-dom's Route Component
+          // to display pages */}
+  
+          {/* Home Page */}
+          <Route exact={true} path='/' component={HomePage} />
+  
+          {/* Shop Page */}
+          <Route exact={true} path='/shop' component={ShopPage} />
+  
+          {/* Sign In/Sign Up Page */}
+          <Route exact={true} path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
